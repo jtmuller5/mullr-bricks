06ee6a41b4ed76e166cf163d3659c700b42df0af
jtmuller5 <47997351+jtmuller5@users.noreply.github.com>
2021-12-31 17:38:04 -0500

    Update medium.dart

--- mason/bricks/rview/__brick__/{{#camelCase}}{{name}}{{/camelCase}}/layouts/medium.dart
+++ mason/bricks/rview/__brick__/{{#camelCase}}{{name}}{{/camelCase}}/layouts/medium.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import './../{{#snakeCase}}{{name}}ViewModel{{/snakeCase}}.dart';
 
-class Medium {{#pascalCase}}{{name}}View{{/pascalCase}} extends ViewModelWidget<{{#pascalCase}}{{name}}ViewModel{{/pascalCase}}> {
+class Medium{{#pascalCase}}{{name}}View{{/pascalCase}} extends ViewModelWidget<{{#pascalCase}}{{name}}ViewModel{{/pascalCase}}> {
 
 @override
 Widget build(BuildContext context, {{#pascalCase}}{{name}}ViewModel{{/pascalCase}} model) {
