9dc9cda1f6c048e1242845c4b170cd6b622942f2
Joseph Muller <47997351+jtmuller5@users.noreply.github.com>
2022-01-01 18:12:05 -0500

    AppRouter

--- mason/bricks/main/__brick__/lib/app/app_router.dart
+++ mason/bricks/main/__brick__/lib/app/app_router.dart
@@ -2,7 +2,8 @@ import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
 import '../shared/guards/check_auth_guard.dart';
 import '../shared/guards/check_connectivity_guard.dart';
-import '../ui/decision/decision_view.dart';
+import '../ui/dashboard/dashboard_view.dart';
+import '../ui/feed/feed_view.dart';
 import '../ui/home/home_view.dart';
 
 import '../ui/shared/notConnected/not_connected_view.dart';
