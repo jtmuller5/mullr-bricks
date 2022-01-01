c5aca15abd1f98cd897b6c386393c6d6cd0f09a4
Joseph Muller <47997351+jtmuller5@users.noreply.github.com>
2022-01-01 18:18:22 -0500

    cleanup

--- mason/bricks/main/__brick__/lib/app/app_router.dart
+++ mason/bricks/main/__brick__/lib/app/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
-import '../shared/guards/check_connectivity_guard.dart';
 import '../ui/dashboard/dashboard_view.dart';
 import '../ui/feed/feed_view.dart';
 import '../ui/home/home_view.dart';
@@ -8,9 +7,7 @@ import '../ui/home/home_view.dart';
 import '../ui/shared/notConnected/not_connected_view.dart';
 import 'app_router.gr.dart';
 
-final appRouter = AppRouter(
-  checkConnectivityGuard: CheckConnectivityGuard(),
-);
+final appRouter = AppRouter();
 
 @AdaptiveAutoRouter(routes: <AutoRoute>[
   AutoRoute(
