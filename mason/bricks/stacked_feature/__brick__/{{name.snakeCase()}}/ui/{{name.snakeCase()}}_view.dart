5f8b136ae2adb509e458040072acc303f4107d98
Joseph Muller <jtmuller5@gmail.com>
2022-11-20 12:50:43 -0500

    brick

--- mason/bricks/stacked_feature/__brick__/{{name.snakeCase()}}/ui/{{name.snakeCase()}}_view.dart
+++ mason/bricks/stacked_feature/__brick__/{{name.snakeCase()}}/ui/{{name.snakeCase()}}_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import '{{name.snakeCase()}}_view_model.dart';
 
 class {{name.pascalCase()}}View extends StatelessWidget {
   const {{name.pascalCase()}}View({Key? key}) : super(key: key);
